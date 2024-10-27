108c0178547d892c0d9c28d42f281115f0a7dc27
unknown <mydulcse62.niter@gmail.com>
2024-10-27 20:25:42 +0600

    change folder and file

---
+++ Project3/index.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  const mobileSidebar = document.querySelector('.mobile-sidebar');
+  mobileSidebar.classList.toggle('show');
+  document.body.classList.toggle('no-scroll');
+}

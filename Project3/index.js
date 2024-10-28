8c7fddea440699407e385f1a1ca6d4612af6b1a1
unknown <mydulcse62.niter@gmail.com>
2024-10-28 06:42:01 +0600

    fix style

--- Project3/index.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-  const mobileSidebar = document.querySelector('.mobile-sidebar');
-  mobileSidebar.classList.toggle('show');
-  document.body.classList.toggle('no-scroll');
-}
